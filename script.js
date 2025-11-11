babf18ddf578e5738c89d3234ac2d1635d8c9673
Tanny2709 <123792042+Tanny2709@users.noreply.github.com>
2025-11-11 17:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
-  // write your code here
+  l = Number(l); // ensure input is a number
+
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < l; i++) {
+    const randIndex = Math.floor(Math.random() * chars.length);
+    result += chars[randIndex];
+  }
+
+  return result;
 }
 
 // Do not change the code below.
